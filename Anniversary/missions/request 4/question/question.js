eeb23cd6eb16652c3bfdaff501fa37fcdc91694a
lntv0407 <thoang0303@gmail.com>
2022-12-20 12:06:54 -0500

    submit

---
+++ Anniversary/missions/request 4/question/question.js
@@ -0,0 +1,9 @@
+let option = "chat";
+let attempt = 0;
+while(option.toUpperCase() !== "BENZEN" && option.toUpperCase() !== "BENZEN ") {
+    attempt++;
+    option = prompt("Chất này là chất gì?");
+}
+    
+alert("Chúc mừng chị gái học hóa đã hoàn thành sau " + attempt + " lượt!!!");
+alert("Chị hãy vào link này để nhận nhiệm vụ tiếp theo nhaaaa!");
